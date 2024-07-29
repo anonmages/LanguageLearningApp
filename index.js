30b4584660cadab1ab266a6f07a817390d6e2ff5
anonmages <163333563+anonmages@users.noreply.github.com>
2024-07-29 16:47:11 +0300

    Init

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import dotenv from 'dotenv';
+dotenv.config();
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
