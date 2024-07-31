f8e44ebbf6dee8727d3724b403336a45e57e5063
anonmages <163333563+anonmages@users.noreply.github.com>
2024-07-31 09:31:47 +0300

    add error catching

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+// ErrorBoundary.js
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    this.setState({ errorInfo });
+    console.log({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary'; // Ensure this path is correct based on your project structure
 import dotenv from 'dotenv';
+
 dotenv.config();
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  rootElement
+);
